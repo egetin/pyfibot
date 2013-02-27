6cf13216650b8f3278391f80f5f44c5e0060a8aa
Erkka M <erkka.makinen@gmail.com>
2013-02-27 20:31:03 +0200

    Auto-oppiin jokerimerkit toimimaan (siirretty vanha versio käyttöön)

--- pyfibot/modules/module_autoop.py
+++ pyfibot/modules/module_autoop.py
@@ -1,154 +1,223 @@
-# -*- coding: utf-8 -*-
 """
+$Id$
+$HeadURL$
+
 Commands:
-.autoop add channel nick!ident@example.org         adds hostmask to channels auto-op list
-.autoop remove channel nick!ident@example.org      removes hostmask from channels auto-op list
-.autoop list <channel>                             lists ops in current channel or channel
-.autoop status                                      lists channels where user is op
+.autoop add/del/status/list
 .op
+
+Nick-format: nick!ident@example.org
+
+Supports adding/deleting multiple autoop-rules:
+.autoop add #testchannel firstNick!ident@example.org secNick!ident@example.org
+
+In add/del #channel needed, if changes done in query. If changes are made in #channel, defaults to current #channel.
+
+config-file in root/modules/module_autooop.conf
+config-format:
+ 'nick!ident@example.org':
+ - '#channelOne'
+ - '#channelTwo'
+
+ 'secNick!ident@example.org':
+ - '#channelTwo'
 """
 
-from __future__ import unicode_literals, print_function, division
-import sqlite3
+import fnmatch
+import yaml
+import os.path
+import sys
 import re
 
-
-COMMANDS = ['add', 'remove', 'list', 'status']
-CHANNEL_PREFIXES = '&#!+'
+oplistfile = os.path.join(sys.path[0], 'modules', 'module_autoop.conf')
+oplist = dict()
 
 
 def init(botconfig):
-    open_DB(True)
+    global oplist
+    if os.path.exists(oplistfile):
+        oplist = yaml.load(file(oplistfile))
 
 
-def open_DB(createTable=False):
-    conn = sqlite3.connect('module_autoop.db')
-    c = conn.cursor()
-    if createTable:
-        c.execute('CREATE TABLE IF NOT EXISTS autoops (channel, hostmask, modes);')
-        conn.commit()
-    return conn, c
+def writeConfig():
+    # open file for writing (note, removes all markings that have been made to config-file prior to .rehash
+    f = open(oplistfile, 'w')
+    yaml.dump(oplist, f, default_flow_style=False)
+    f.close()
 
 
-def add_op(channel, hostmask, modes='o'):
-    if not get_op_status(channel, hostmask):
-        conn, c = open_DB()
-        c.execute('INSERT INTO autoops VALUES (?, ?, ?);', (channel, hostmask, modes))
-        conn.commit()
-        conn.close()
-        return True
-
-
-def remove_op(channel, hostmask):
-    if get_op_status(channel, hostmask):
-        conn, c = open_DB()
-        c.execute('DELETE FROM autoops WHERE channel = ? AND hostmask = ?;', (channel, hostmask))
-        conn.commit()
-        conn.close()
-        return True
-
-
-def get_op_status(channel, hostmask):
-    conn, c = open_DB()
-    c.execute('SELECT * FROM autoops WHERE channel = ? AND hostmask = ? LIMIT 1;', (channel, hostmask))
-    if c.fetchone():
-        retval = True
-    else:
-        retval = False
-    conn.close()
-    return retval
-
-
-def get_user_channels(hostmask):
-    conn, c = open_DB()
-    c.execute('SELECT channel FROM autoops WHERE hostmask = ?;', (hostmask,))
-    rows = c.fetchall()
-    conn.close()
-    return rows
-
-
-def get_ops(channel):
-    conn, c = open_DB()
-    c.execute('SELECT hostmask FROM autoops WHERE channel = ?;', (channel,))
-    rows = c.fetchall()
-    conn.close()
-    return rows
-
-
-def check_hostmask(hostmask):
-    rg = re.compile('.*?(!).*?(@).*?', re.IGNORECASE | re.DOTALL)  # regex for checking hostmask format
-    if re.match(rg, hostmask):
-        return True
-    return False
-
-
-def op_user(bot, user, channel):
-    nick = getNick(user)
-    bot.log('auto-opping %s on %s' % (user, channel))
-    bot.mode(channel, True, 'o', user=nick)
+def isAutoOppable(user, channel):
+    for pattern in oplist:
+        if fnmatch.fnmatch(user, pattern):
+            if channel in oplist[pattern]:
+                return True
 
 
 def handle_userJoined(bot, user, channel):
-    if isAdmin(user) or get_op_status(channel, user):
-        op_user(bot, user, channel)
+    if isAdmin(user) or isAutoOppable(user, channel):
+        nick = getNick(user)
+        bot.log('auto-opping %s on %s' % (user, channel))
+        bot.mode(channel, True, 'o', user=nick)
 
 
-def command_autoop(bot, user, channel, args):
-    """Usage: .autoop [add|remove|list|status]"""
-
-    if not args:
-        return bot.say(channel, 'Valid commands are %s' % ', '.join(map(str, COMMANDS)))
-
-    args = args.split()
-    command = args[0]
-    if command not in COMMANDS:
-        return bot.say(channel, 'Invalid command, valid commands are %s' % ', '.join(map(str, COMMANDS)))
-
-    if command == 'list':
-        if len(args) > 1:
-            list_channel = args[1]
-        else:
-            list_channel = channel
-        if list_channel[0] not in CHANNEL_PREFIXES:
-            return bot.say(channel, 'Channel name must start with one of "%s"' % CHANNEL_PREFIXES)
-
-        ops = get_ops(list_channel)
-        if ops:
-            return bot.say(channel, '%s ops: %s' % (list_channel, ', '.join(str(i[0]) for i in ops)))
-        return bot.say(channel, "%s doesn't have any ops." % list_channel)
-
-    elif command == 'status':
-        if channel[0] not in CHANNEL_PREFIXES:
-            channel_list = get_user_channels(user)
-            if channel_list:
-                return bot.say(channel, "You're auto-opped on %s" % ', '.join(str(i[0]) for i in channel_list))
-            return bot.say(channel, "You're not auto-opped on any channel.")
-        else:
-            if get_op_status(channel, user):
-                return bot.say(channel, "You're op in %s." % channel)
-            return bot.say(channel, "You're not an op in %s." % channel)
-
+def handle_args(args, channel):
+    message = ''
+    nicks = list()
+    rg = re.compile('.*?(!).*?(@).*?', re.IGNORECASE | re.DOTALL)    # regex for validating nick format
+    if re.match(rg, args[0].strip()) is None and args[0].strip()[0] == '#':
+        op_channel = args[0].strip()
+        for nick in args[1:]:
+            if re.match(rg, nick):
+                nicks.append(nick)
+            else:
+                message += '%s not in correct format. ' % nick
     else:
-        if isAdmin(user):
-            if len(args) < 3:
-                return bot.say(channel, 'Command must have channel and hostmask as arguments.')
+        op_channel = channel
+        for nick in args[0:]:
+            if re.match(rg, nick):
+                nicks.append(nick)
+            else:
+                message += '%s not in correct format. ' % nick
+    return nicks, op_channel, message
 
-            if not (args[1][0] in CHANNEL_PREFIXES and check_hostmask(args[2])):
-                return bot.say(channel, 'Channel name must start with one of "%s" and hostmask must be in format nick!ident@hostma.sk' % CHANNEL_PREFIXES)
 
-            if command == 'add':
-                if add_op(args[1], args[2]):
-                    return bot.say(channel, 'OP added.')
-                return bot.say(channel, 'Adding OP failed, is user already auto-opped?')
-            elif command == 'remove':
-                if remove_op(args[1], args[2]):
-                    return bot.say(channel, 'OP removed.')
-                return bot.say(channel, 'Removing OP failed, is user the user auto-opped?')
+def autoop_status(user, channel, args):
+    nick = getNick(user)
+    if isAdmin(user):
+        return '%s: you are %s on %s, status: bot admin' % (nick, user, channel)
+    elif isAutoOppable(user, channel):
+        return '%s: you are %s on %s, status: auto-op' % (nick, user, channel)
+    else:
+        return '%s: you are %s on %s: status: -' % (nick, user, channel)
+
+
+def autoop_add(user, channel, args):
+    if isAdmin(user):
+        nicks = list()
+        message = ''
+        if len(args) > 0:
+            nicks, op_channel, message = handle_args(args, channel)
+            if cmp(op_channel, user) != 0:
+                for nick in nicks:
+                    if nick in oplist:    # if already in oplist, just add to existing list
+                        if op_channel in oplist[nick]:    # if not in nicks channel list, add
+                            message += 'Auto-op for %s already in %s. ' % (nick, op_channel)
+                        else:
+                            message += 'Auto-op for %s in %s added. ' % (nick, op_channel)
+                            oplist[nick].append(op_channel)
+                    else:
+                        message += 'Auto-op for %s in %s added. ' % (nick, op_channel)
+                        oplist[nick] = list()
+                        oplist[nick].append(op_channel)
+                writeConfig()
+            else:
+                message = '#channel always needed when making changes in query!'
+            return message
         else:
-            return bot.say(channel, 'You must be bot admin to use this feature.')
+            return 'Usage: .autoop add [#channel] nick!ident@hostma.sk'
+
+
+def autoop_del(user, channel, args):
+    if isAdmin(user):
+        message = ''
+        if len(args) > 0:
+            nicks, op_channel, message = handle_args(args, channel)
+            if cmp(op_channel, user) != 0:
+                for nick in nicks:
+                    if nick in oplist:
+                        if op_channel in oplist[nick]:  # if in nicks channel list, remove
+                            message += 'Auto-op for %s in %s removed. ' % (nick, op_channel)
+                            oplist[nick].remove(op_channel)
+                            if len(oplist[nick]) == 0:
+                                del oplist[nick]
+                        else:
+                            message += 'No auto-op for %s in %s. ' % (nick, op_channel)
+                    else:
+                        message += 'No auto-op for %s in %s. ' % (nick, op_channel)
+                writeConfig()
+            else:
+                message = '#channel always needed when making changes in query!'
+            return message
+        else:
+            return 'Usage: .autoop del [#channel] nick!ident@hostma.sk'
+
+
+def autoop_list(user, channel, args):
+    if isAdmin(user):
+        i = 0
+        rg = re.compile('.*?(!).*?(@).*?', re.IGNORECASE | re.DOTALL)  # regex for validating nick format
+        if (len(args) == 0 or (re.match(rg, args[0].strip()) is None and args[0].strip()[0] == '#')) and args[0] != 'me':
+            if len(args) == 0:
+                op_channel = channel
+            elif re.match(rg, args[0].strip()) is None and args[0].strip()[0] == '#':
+                op_channel = args[0].strip()
+            message = 'Auto-ops in %s: ' % op_channel
+            for nick, channels in oplist.items():
+                for chan in channels:
+                    if chan == op_channel:
+                        message += nick + ' '
+                        i = i + 1
+            if i != 0:
+                return message
+            else:
+                return '%s has no auto-ops.' % op_channel
+        elif re.match(rg, args[0].strip()) or args[0] == 'me':
+            if args[0] != 'me':
+                nick = args[0].strip()
+            else:
+                nick = user
+            message = '%s auto-op in: ' % nick
+            if nick in oplist:
+                for chan in oplist[nick]:
+                    message += chan + ' '
+                    i = i + 1
+            if i != 0:
+                return message
+            else:
+                return '%s not auto-op anywhere.' % nick
+        else:
+            return 'Usage: .autoop_list [#channel _or_ nick!ident@hostma.sk]'
+    else:
+        nick = user
+        i = 0
+        message = '%s auto-op in: ' % nick
+        if nick in oplist:
+            for chan in oplist[nick]:
+                message += chan + ' '
+                i = i + 1
+        if i != 0:
+            return message
+        else:
+            return '%s not auto-op anywhere.' % nick
+
+
+def make_valid_commands_msg(commands):
+    message = 'Valid commands are: '
+    for command in commands:
+        message += command
+        message += ', '
+    message = message[:-2]
+    message += '.'
+    return message
 
 
 def command_op(bot, user, channel, args):
-    """Get op status from bot if you are authorized"""
-    if isAdmin(user) or get_op_status(channel, user):
-        op_user(bot, user, channel)
+    if isAdmin(user) or isAutoOppable(user, channel):
+        nick = getNick(user)
+        bot.log('opping %s by request on %s' % (user, channel))
+        bot.mode(channel, True, 'o', user=nick)
+
+
+def command_autoop(bot, user, channel, args):
+    commands = {'add': autoop_add, 'del': autoop_del, 'list': autoop_list, 'status': autoop_status}
+    args = args.split()
+    if len(args) > 0:
+        command = args[0]
+        if command in commands:
+            args.remove(command)
+            message = commands[command](user, channel, args)
+        else:
+            message = make_valid_commands_msg(commands)
+    else:
+        message = make_valid_commands_msg(commands)
+    return bot.say(channel, message)
